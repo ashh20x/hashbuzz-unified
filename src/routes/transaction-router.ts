bd1aa48cfb34aa42c60208585ad49f081a51aa51
curepathDev <curepathology.dev@gmail.com>
2022-08-21 00:48:09 +0530

    Schema updated for smartcontract table

--- src/routes/transaction-router.ts
+++ src/routes/transaction-router.ts
@@ -1,18 +1,20 @@
 import { Request, Response, Router } from "express";
+import { body , validationResult } from "express-validator";
 import statusCodes from "http-status-codes";
 // import { topUpHandler } from "@controller/transaction.controller";
 
 // Constants
 const router = Router();
-const { OK } = statusCodes;
+const { OK, BAD_REQUEST } = statusCodes;
 // Paths
-export const p = {
-  topUp: "/top-up",
-} as const;
 
-router.post(p.topUp, topUpHandler);
+router.post("/top-up", body("amount").isFloat(), topUpHandler);
 
 function topUpHandler(req: Request, res: Response) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(BAD_REQUEST).json({ errors: errors.array() });
+  }
   res.status(OK).json({ response: "success" });
 }
 
