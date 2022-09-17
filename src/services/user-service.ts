891da82210cb0081e4214de35ebf8004bc486bd3
Om Prakash sah <omkolkata33@gmail.com>
2022-09-18 00:29:02 +0530

    Update for add-campaign operation

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -17,27 +17,38 @@ const getUserById = async (id?: number | bigint) => {
 };
 
 const topUp = async (id: number | bigint, amounts: number, operation: "increment" | "decrement" | "update") => {
-  let available_budget;
-
   if (operation === "increment")
-    available_budget = {
-      increment: amounts,
-    };
+    return await prisma.user_user.update({
+      where: {
+        id,
+      },
+      data: {
+        available_budget: {
+          increment: amounts,
+        },
+      },
+    });
   else if (operation === "decrement")
-    available_budget = {
-      decrement: amounts,
-    };
-  else available_budget = amounts;
-
+    return await prisma.user_user.update({
+      where: {
+        id,
+      },
+      data: {
+        available_budget: {
+          decrement: amounts,
+        },
+      },
+    });
+  else
+    return await prisma.user_user.update({
+      where: {
+        id,
+      },
+      data: {
+        available_budget: amounts,
+      },
+    });
   //? Perform DN Query
-  return await prisma.user_user.update({
-    where: {
-      id,
-    },
-    data: {
-      available_budget,
-    },
-  });
 };
 
 export default {
