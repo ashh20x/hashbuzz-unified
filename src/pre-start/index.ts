8e2ea4272688a5f4c97e29896178f37fced9f4bc
Om Prakash sah <omkolkata33@gmail.com>
2022-09-18 14:04:35 +0530

    Tweeter api pagination testing done

--- src/pre-start/index.ts
+++ src/pre-start/index.ts
@@ -3,14 +3,22 @@
  * This is useful for environment variables, command-line arguments, and cron-jobs.
  */
 
-import dotenv from 'dotenv';
-import { taskEveryMinute , taskEverySixDay } from './cronJob';
-
+import dotenv from "dotenv";
+import { taskEveryMinute, taskEverySixDay } from "./cronJob";
+// import tweeterApi from "@shared/twitterAPI";
+// import { writeFileSync } from "fs";
 
 (() => {
-    // Setup command line options
-    dotenv.config();
-    taskEveryMinute.start();
-    taskEverySixDay.start();
-    // await getAllReplies("1559560034744381442");
+  // Setup command line options
+  dotenv.config();
+  taskEveryMinute.start();
+  taskEverySixDay.start();
+  // (async () => {
+  //   const allLikedUsers = await tweeterApi.getAllUsersWhoLikedOnTweetId("1568884114308349952");
+  //   const reTweetData = await tweeterApi.getAllRetweetOfTweetId("1568884114308349952");
+  //   const QuotedTweets = await tweeterApi.getAllUsersWhoQuotedOnTweetId("1569953613073764352")
+  //   writeFileSync("./logs/tweet-stats-likes-1568884114308349952.json", JSON.stringify(allLikedUsers), "utf8");
+  //   writeFileSync("./logs/tweet-stats-retweets-1568884114308349952.json", JSON.stringify(reTweetData), "utf8");
+  //   writeFileSync("./logs/tweet-stats-quotes-1569953613073764352.json", JSON.stringify(QuotedTweets), "utf8");
+  // })();
 })();
--- src/shared/functions.ts
+++ src/shared/functions.ts
@@ -25,10 +25,10 @@ const getRandomInt = (): number => {
  * Calculate total budget spent for the a twitter campaign.
  *
  */ const calculateTotalSpent = (stats: Required<TwitterStats>, rewards: RewardCatalog): number => {
-  const { like_count, quote_count, retweet_count } = stats;
-  const { like_reward, quote_reward, retweet_reward } = rewards;
+  const { like_count, quote_count, retweet_count, reply_count } = stats;
+  const { like_reward, quote_reward, retweet_reward, reply_reward } = rewards;
   let totalSpentAmount = 0;
-  totalSpentAmount = like_count * like_reward + retweet_count * retweet_reward + quote_count * quote_reward;
+  totalSpentAmount = like_count * like_reward + retweet_count * retweet_reward + quote_count * quote_reward + reply_count * reply_reward;
   return totalSpentAmount;
 };
 
