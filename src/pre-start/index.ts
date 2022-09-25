5238fd83a41428af614cfee2028282f6866f4dbb
Om Prakash sah <omkolkata33@gmail.com>
2022-09-25 17:22:23 +0530

    pre start task is ended

--- src/pre-start/index.ts
+++ src/pre-start/index.ts
@@ -3,9 +3,7 @@
  * This is useful for environment variables, command-line arguments, and cron-jobs.
  */
 
-import cronService from "@services/cronTasks-service";
 import dotenv from "dotenv";
-import twitterAPI from "@shared/twitterAPI";
 import fs from "fs";
 import { taskAtEveryMidNight, taskEveryTwoMinute } from "./cronJob";
 
@@ -17,16 +15,16 @@ import { taskAtEveryMidNight, taskEveryTwoMinute } from "./cronJob";
   taskAtEveryMidNight.start();
 
   //reply tasks
-  (async () => {
-    await cronService.scheduleExpiryTasks();
+  // (async () => {
+    // await cronService.scheduleExpiryTasks();
     // await cronService.checkForRepliesAndUpdateEngagementsData()
     //   const allLikedUsers = await tweeterApi.getAllUsersWhoLikedOnTweetId("1568884114308349952");
     //   const reTweetData = await tweeterApi.getAllRetweetOfTweetId("1568884114308349952");
-      const QuotedTweets = await twitterAPI.getAllUsersWhoQuotedOnTweetId("1569953613073764352")
+      // const QuotedTweets = await twitterAPI.getAllUsersWhoQuotedOnTweetId("1569953613073764352")
     // const getAllReplies = await tweeterApi.getAllReplies("1571866729558724609");
     // writeFileSync("./logs/tweet-stats-replies-1571866729558724609.json", JSON.stringify(getAllReplies), "utf8");
     //   writeFileSync("./logs/tweet-stats-likes-1568884114308349952.json", JSON.stringify(allLikedUsers), "utf8");
     //   writeFileSync("./logs/tweet-stats-retweets-1568884114308349952.json", JSON.stringify(reTweetData), "utf8");
-      fs.writeFileSync("./logs/tweet-stats-quotes-1569953613073764352.json", JSON.stringify(QuotedTweets), "utf8");
-  })();
+      // fs.writeFileSync("./logs/tweet-stats-quotes-1569953613073764352.json", JSON.stringify(QuotedTweets), "utf8");
+  // })();
 })();
