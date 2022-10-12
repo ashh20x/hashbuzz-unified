26d59551ce039b52700613924473cfdf4a120d4e
Om Prakash sah <omkolkata33@gmail.com>
2022-10-12 17:36:47 +0530

    brand handle connection

--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -1,5 +1,5 @@
 import { Router, Response, Request } from "express";
-import { twitterAuthUrl } from "@services/auth-service";
+import { authLogin, twitterAuthUrl } from "@services/auth-service";
 import HttpStatusCodes from "http-status-codes";
 import prisma from "@shared/prisma";
 import { user_user } from "@prisma/client";
@@ -7,99 +7,130 @@ import twitterAPI from "@shared/twitterAPI";
 import userService from "@services/user-service";
 import moment from "moment";
 import { generateAccessToken, generateRefreshToken } from "@services/authToken-service";
+import auth from "@middleware/auth";
+import logger from "jet-logger";
 
 const authRouter = Router();
-const { OK  , TEMPORARY_REDIRECT} = HttpStatusCodes;
+const { OK, TEMPORARY_REDIRECT } = HttpStatusCodes;
 
 authRouter.get("/twitter-login", (req: Request, res: Response) => {
   (async () => {
-    console.log("twitter-login:::")
-    const url = await twitterAuthUrl(`${process.env.TWITTER_CALLBACK_HOST!}/auth/twitter-return/`);
+    console.log("twitter-login:::");
+    const url = await twitterAuthUrl({ callbackUrl: `${process.env.TWITTER_CALLBACK_HOST!}/auth/twitter-return/` });
+    return res.status(OK).json({ url });
+  })();
+});
+
+authRouter.get("/brand-handle", auth.isHavingValidAuthToken, (req: Request, res: Response) => {
+  (async () => {
+    console.log("twitter-login:::");
+    const url = await twitterAuthUrl({
+      callbackUrl: `${process.env.TWITTER_CALLBACK_HOST!}/auth/business-twitter-return/`,
+      isBrand: true,
+      business_owner_id: req.currentUser?.id,
+    });
     return res.status(OK).json({ url });
   })();
 });
 
 authRouter.get("/twitter-return", (req: Request, res: Response) => {
   (async () => {
-    console.log({params:req.params, query:req.query})
-    // Extract tokens from query string
-    const oauth_token = req.query.oauth_token as any as string;
-    const oauth_verifier = req.query.oauth_verifier as any as string;
-    console.log("getting Token Token=::" , oauth_token);
-    // Get the saved oauth_token_secret from session
-    // const { oauth_token_secret } = req.session;
-    const authtoken = await prisma.user_twitterlogintemp.findFirst({
-      where: {
-        oauth_token,
-      },
-    });
+    try {
+      // Extract tokens from query string
+      const oauth_token = req.query.oauth_token as any as string;
+      const oauth_verifier = req.query.oauth_verifier as any as string;
 
-    const { oauth_token_secret } = authtoken!;
-
-    console.log("getting oauth_token_secret=::" , oauth_token_secret);
-
-    if (!oauth_token || !oauth_verifier || !oauth_token_secret) {
-      return res.status(400).send("You denied the app or your session expired!");
-    }
-
-    // Obtain the persistent tokens
-    // Create a client from temporary tokens
-    const client = twitterAPI.tweeterApiForUser({ accessSecret: oauth_token_secret, accessToken: oauth_token });
-
-    client
-      .login(oauth_verifier)
-      .then(async ({ client: loggedClient, accessToken, accessSecret }) => {
-        const meUser = await client.v2.me();
-        const { username, id } = meUser.data;
-        let user: user_user;
-        const existinguser = await userService.getUserByUserName(username);
-        if (!existinguser) {
-          user = await prisma.user_user.create({
-            data: {
-              personal_twitter_handle: username,
-              username: username,
-              personal_twitter_id: id,
-              twitter_access_token: accessToken,
-              twitter_access_token_secret: accessSecret,
-              available_budget: 0,
-              is_superuser: false,
-              is_active: true,
-              is_staff: false,
-              first_name: "",
-              last_name: "",
-              email: "",
-              password: "",
-              date_joined: moment().toISOString(),
-            },
-          });
-        } else {
-          user = await prisma.user_user.update({
-            where: {
-              username,
-            },
-            data: {
-              twitter_access_token: accessToken,
-              twitter_access_token_secret: accessSecret,
-            },
-          });
-        }
-        // ?token={token.key}&user_id={user.id}
-        const token = generateAccessToken(user);
-        const refreshToken = await generateRefreshToken(user);
-        res.writeHead( TEMPORARY_REDIRECT , {
-          'Location':`${process.env.FRONTEND_URL!}?token=${token}&user_id=${user.id}&refresh_token=${refreshToken}`
+      // Obtain the persistent tokens
+      // Create a client from temporary tokens
+      const { loginResult } = await authLogin({ oauth_token, oauth_verifier });
+      const { client: loggedClient, accessToken, accessSecret } = loginResult;
+      const meUser = await loggedClient.v2.me();
+      const { username, id } = meUser.data;
+      let user: user_user;
+      const existinguser = await userService.getUserByUserName(username);
+      if (!existinguser) {
+        user = await prisma.user_user.create({
+          data: {
+            personal_twitter_handle: username,
+            username: username,
+            personal_twitter_id: id,
+            twitter_access_token: accessToken,
+            twitter_access_token_secret: accessSecret,
+            available_budget: 0,
+            is_superuser: false,
+            is_active: true,
+            is_staff: false,
+            first_name: "",
+            last_name: "",
+            email: "",
+            password: "",
+            date_joined: moment().toISOString(),
+          },
         });
-        res.end();
-        //check user is existing or not;
-
-        // loggedClient is an authenticated client in behalf of some user
-        // Store accessToken & accessSecret somewhere
-      })
-      .catch((error) => {
-        console.log(error);
-       return res.status(403).send("Invalid verifier or access tokens!")
+      } else {
+        user = await prisma.user_user.update({
+          where: {
+            username,
+          },
+          data: {
+            twitter_access_token: accessToken,
+            twitter_access_token_secret: accessSecret,
+          },
+        });
+      }
+      // ?token={token.key}&user_id={user.id}
+      const token = generateAccessToken(user);
+      const refreshToken = await generateRefreshToken(user);
+      res.writeHead(TEMPORARY_REDIRECT, {
+        Location: `${process.env.FRONTEND_URL!}?token=${token}&user_id=${user.id}&refresh_token=${refreshToken}`,
       });
+      res.end();
+    } catch (error) {
+      logger.err(error.message);
+      const message:string = error.message  as string;
+      res.writeHead(TEMPORARY_REDIRECT, {
+        Location: `${process.env.FRONTEND_URL!}?brandConnection=fail&message=${message}`,
+      });
+      res.end();
+    }
   })();
 });
 
+authRouter.get("/business-twitter-return", (req: Request, res: Response) => {
+  (async () => {
+    try {
+      // Extract tokens from query string
+      const oauth_token = req.query.oauth_token as any as string;
+      const oauth_verifier = req.query.oauth_verifier as any as string;
+
+      // Obtain the persistent tokens
+      // Create a client from temporary tokens
+      const { loginResult, business_owner_id } = await authLogin({ oauth_token, oauth_verifier });
+      const { client: loggedClient, accessToken, accessSecret } = loginResult;
+      const meUser = await loggedClient.v2.me();
+      const { username, id } = meUser.data;
+      const user = await prisma.user_user.update({
+        where: {
+          id: business_owner_id!,
+        },
+        data: {
+          business_twitter_handle: username,
+          business_twitter_access_token: accessToken,
+          business_twitter_access_token_secret: accessSecret,
+        },
+      });
+      res.writeHead(TEMPORARY_REDIRECT, {
+        Location: `${process.env.FRONTEND_URL!}?brandConnection=success&handle=${username}`,
+      });
+      res.end();
+    } catch (error) {
+      logger.err(error.message);
+      const message:string = error.message  as string;
+      res.writeHead(TEMPORARY_REDIRECT, {
+        Location: `${process.env.FRONTEND_URL!}?brandConnection=fail&message=${message}`,
+      });
+      res.end();
+    }
+  })();
+});
 export default authRouter;
--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,28 +1,52 @@
 import prisma from "@shared/prisma";
+import twitterAPI from "@shared/twitterAPI";
 import moment from "moment";
 import { TwitterApi } from "twitter-api-v2";
 
 const client = new TwitterApi({ appKey: process.env.TWITTER_API_KEY!, appSecret: process.env.TWITTER_API_SECRET! });
 
-export const twitterAuthUrl = async (callbackUrl: string) => {
-  console.log(callbackUrl);
-  // const authLink = await client.generateAuthLink(CALLBACK_URL);
-
+export const twitterAuthUrl = async ({callbackUrl , isBrand , business_owner_id}:{callbackUrl: string, isBrand?:boolean , business_owner_id?:bigint|null}) => {
   // By default, oauth/authenticate are used for auth links, you can change with linkMode
   // property in second parameter to 'authorize' to use oauth/authorize
-  const authLink = await client.generateAuthLink(callbackUrl, { linkMode: "authorize" });
+  console.log(callbackUrl);
+  let authLink;
+  if (isBrand) authLink = await client.generateAuthLink(callbackUrl);
+  else authLink = await client.generateAuthLink(callbackUrl, { linkMode: "authorize" });
   const { url, oauth_callback_confirmed, oauth_token, oauth_token_secret } = authLink;
 
-  console.log("Saving Token=::" , oauth_token)
+  console.log("Saving Token=::", oauth_token);
 
   await prisma.user_twitterlogintemp.create({
     data: {
       oauth_callback_confirmed: oauth_callback_confirmed === "true" ? true : false,
       oauth_token,
       oauth_token_secret,
-      created_at:moment().toISOString()
+      created_at: moment().toISOString(),
+      business_owner_id
     },
   });
 
   return url;
+};
+
+export const authLogin = async ({ oauth_token, oauth_verifier }: { oauth_verifier: string; oauth_token: string }) => {
+  const authtoken = await prisma.user_twitterlogintemp.findFirst({
+    where: {
+      oauth_token,
+    },
+  });
+
+  const { oauth_token_secret , business_owner_id } = authtoken!;
+
+  console.log("getting oauth_token_secret=::", oauth_token_secret);
+
+  if (!oauth_token || !oauth_verifier || !oauth_token_secret) {
+    throw Error("You denied the app or your session expired!");
+  }
+
+  // Obtain the persistent tokens
+  // Create a client from temporary tokens
+  const client = twitterAPI.tweeterApiForUser({ accessSecret: oauth_token_secret, accessToken: oauth_token });
+  const loginResult = await client.login(oauth_verifier)
+  return {loginResult , business_owner_id};
 };
