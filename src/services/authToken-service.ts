a2efd1c9e634c89aa1575208349e82a4f0441a70
Om Prakash sah <omkolkata33@gmail.com>
2022-10-12 19:09:50 +0530

    upsert method for unique record

--- src/services/authToken-service.ts
+++ src/services/authToken-service.ts
@@ -6,17 +6,21 @@ const refreshSecret = process.env.J_REFRESH_TOKEN_SECRET;
 
 export const generateAccessToken = (user: user_user) => {
   const { id, username, personal_twitter_id } = user;
-  return jwt.sign({ id:id.toString(), username, personal_twitter_id }, accessSecret!, { expiresIn: "20m" });
+  return jwt.sign({ id: id.toString(), username, personal_twitter_id }, accessSecret!, { expiresIn: "20m" });
 };
 
 export const generateRefreshToken = async (user: user_user) => {
   const { id, username, personal_twitter_id } = user;
-  const refreshToken = jwt.sign({ id:id.toString(), username, personal_twitter_id }, refreshSecret!, { expiresIn: "30m" });
-  await prisma.authtoken_token.create({
-    data: {
+  const refreshToken = jwt.sign({ id: id.toString(), username, personal_twitter_id }, refreshSecret!, { expiresIn: "30m" });
+  await prisma.authtoken_token.upsert({
+    where: { user_id: id },
+    create: {
       user_id: id,
       key: refreshToken,
       created: new Date().toISOString(),
+    },
+    update: {
+      key: refreshToken,
     },
   });
 
