868c2835dde145b51ff55878c289b7c0d064a0e1
Om Prakash sah <omkolkata33@gmail.com>
2023-01-20 13:32:18 +0530

    logo Update

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Theme from "./theme/Theme";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Main } from "./screens/Main";
-import { CreateTwitterCard } from "./screens/CreateTwitterCard";
+// import { CreateTwitterCard } from "./screens/CreateTwitterCard";
 import { OnBoarding } from "./screens/OnBoarding";
 import { AdminPanel } from "./screens/AdminPanel";
 import { Template } from "./screens/Template";
 import { Invoice } from "./screens/Invoice";
 import { ProtectedRoute } from "./APIConfig/AuthGuard";
 import { ToastContainer } from 'react-toastify';
+import {Dashboard} from "./Ver2Designs"
 
 const App =() => {
 
@@ -20,7 +21,7 @@ const App =() => {
         <Theme>
           <Routes>
             <Route path="/" exact element={<ProtectedRoute><Main /></ProtectedRoute>} />
-            <Route path="/dashboard" exact element={<ProtectedRoute><CreateTwitterCard /></ProtectedRoute>} />
+            <Route path="/dashboard" exact element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
             <Route path="/campaign" exact element={<ProtectedRoute><Template /></ProtectedRoute>} />
             <Route path="/invoice" exact element={<ProtectedRoute><Invoice /></ProtectedRoute>} />
             <Route path="/onboarding" exact element={<ProtectedRoute><OnBoarding /></ProtectedRoute>} />
