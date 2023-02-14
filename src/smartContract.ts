990f2301b72323c889cab8682288ca43fc28e892
om2345 <omprakashs@capitalnumbers.com>
2023-02-14 12:16:22 +0530

    Smart contract update

--- src/smartContract.ts
+++ src/smartContract.ts
@@ -1,5 +1,5 @@
 import { readFileSync } from "fs";
 
-export const contractByteCode = readFileSync("SmartContract/build/Hashbuzz_sol_Hashbuzz.bin");
-export const contractAbi = JSON.parse(readFileSync("SmartContract/build/Hashbuzz_sol_Hashbuzz.abi", "utf8"));
+export const contractByteCode = readFileSync("SmartContract/build/Hashbuzz_sol_HashbuzzV2.bin");
+export const contractAbi = JSON.parse(readFileSync("SmartContract/build/Hashbuzz_sol_HashbuzzV2.abi", "utf8"));
 
