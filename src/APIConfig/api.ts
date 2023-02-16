caff88c04fabe7b0d19e07802a8fa3edbcfd9bb7
Om Prakash sah <omkolkata33@gmail.com>
2023-02-16 20:20:21 +0530

    api new architecture is introduced

--- src/APIConfig/api.ts
+++ src/APIConfig/api.ts
@@ -1,9 +1,24 @@
 import axios, { AxiosResponse } from "axios";
 import { AuthCred, CurrentUser, LogoutResponse } from "../types";
-import { dAppApiURL, getCookie, forceLogout } from "../Utilities/Constant";
+import {  forceLogout } from "../Utilities/Constant";
+
+export const getCookie = (cname:string) => {
+  let name = cname + "=";
+  let ca = document.cookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+};
 
 const instance = axios.create({
-  baseURL: dAppApiURL,
+  baseURL: process.env.REACT_APP_DAPP_API,
   timeout: 15000,
   headers: {
     Authorization: `Token ${getCookie("token")}`,
