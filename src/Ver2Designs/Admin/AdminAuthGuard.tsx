127e3b8d2dd8901c5d396429ed323cf74763f8c1
Om Prakash sah <omkolkata33@gmail.com>
2023-07-30 04:49:37 +0530

    Admin auth guard fixed

--- src/Ver2Designs/Admin/AdminAuthGuard.tsx
+++ src/Ver2Designs/Admin/AdminAuthGuard.tsx
@@ -2,13 +2,18 @@ import { useCookies } from "react-cookie";
 import { useStore } from "../../Store/StoreProvider";
 import PageNotfound from "../Pages/PageNotfound";
 import AdminAuth from "./AdminAuth";
+// import { useStore } from "../Store/StoreProvider";
 
 const AdminAuthGuard = ({ children }: { children: JSX.Element }) => {
   const [cookies] = useCookies(["aSToken", "adminToken"]);
   const store = useStore();
-  if (!cookies.aSToken || (store?.currentUser?.role && !["ADMIN", "SUPER_ADMIN"].includes(store?.currentUser?.role))) return <PageNotfound />;
-  if (!cookies.adminToken) return <AdminAuth />;
-  return children;
+
+  if (cookies.aSToken && store?.ping.status && store.currentUser?.role && ["ADMIN", "SUPER_ADMIN"].includes(store.currentUser?.role)) {
+    if (!cookies.adminToken) return <AdminAuth />;
+    else return children;
+  }
+
+  return <PageNotfound />;
 };
 
 export default AdminAuthGuard;
