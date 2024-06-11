a6532a30d9ce12887cfa48cfd36298d6fa5e3082
Rajat K <0112.taskbar@gmail.com>
2024-06-12 00:22:31 +0530

    Redis setup

---
+++ src/services/redis-servie.ts
@@ -0,0 +1,43 @@
+// RedisCRUD.ts
+
+import { createClient, RedisClientType } from 'redis';
+import util from "util"
+
+
+class RedisClient {
+    private client: RedisClientType;
+
+    constructor() {
+        /** For live server */
+
+        // this.client = createClient({
+        //     url: 'redis://alice:foobared@awesome.redis.server:6380'
+        // });
+        
+        this.client = createClient(); // Initialize Redis client
+        this.client.on('error', (err) => console.error('Redis Client Error', err));
+    }
+
+    // Create a new record
+    async create(key: string, value: string): Promise<void> {
+        await this.client.set(key, value);
+    }
+
+    // Read a record by key
+    async read(key: string): Promise<string | null> {
+        const getAsync = util.promisify(this.client.get).bind(this.client);
+        return await getAsync(key);
+    }
+
+    // Update a record by key
+    async update(key: string, value: string): Promise<void> {
+        await this.client.set(key, value);
+    }
+
+    // Delete a record by key
+    async delete(key: string): Promise<void> {
+        await this.client.del(key);
+    }
+}
+
+export default RedisClient;
