27ae253988281c7ba4a734a5c6c0593af0d54a7a
Rajat K <0112.taskbar@gmail.com>
2024-08-28 23:14:21 +0530

    Deployemnt fror remove handle

--- src/Ver2Designs/Pages/Dashboard/DashboardAdmin/AdminViews.tsx
+++ src/Ver2Designs/Pages/Dashboard/DashboardAdmin/AdminViews.tsx
@@ -181,7 +181,7 @@ export const AdminViews = () => {
                   </TableCell>
                   <TableCell>
                     {userInview?.personal_twitter_handle ?? "NA"} 
-                    <IconButton title="Remove this handle" size="small" color="error" disabled={api.isLoading || Boolean(userInview?.personal_twitter_handle)} sx={{marginLeft:2}} 
+                    <IconButton title="Remove this handle" size="small" color="error" disabled={api.isLoading || !Boolean(userInview?.personal_twitter_handle)} sx={{marginLeft:2}} 
                     onClick={() => handlePersonalHandleRemove(Number(userInview?.id))}>
                       <Delete fontSize="inherit"/>
                     </IconButton>
@@ -194,7 +194,7 @@ export const AdminViews = () => {
                   </TableCell>
                   <TableCell>
                     {userInview?.business_twitter_handle ?? "NA"}
-                    <IconButton title="Remove this buesness handle" size="small" color="error" disabled={api.isLoading || Boolean(userInview?.business_twitter_handle)}
+                    <IconButton title="Remove this buesness handle" size="small" color="error" disabled={api.isLoading || !Boolean(userInview?.business_twitter_handle)}
                     sx={{marginLeft:2}} 
                     onClick={() => handlebizHandleRemove(Number(userInview?.id))}>
                       <Delete fontSize="inherit"/>
