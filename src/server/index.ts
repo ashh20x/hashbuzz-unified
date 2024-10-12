1c352d1cd3d6dea3ea67a46be9d44f823d0e3dc0
Om Prakash sah <omkolkata33@gmail.com>
2024-10-12 11:33:38 +0530

    Rate Limit improved

--- src/server/index.ts
+++ src/server/index.ts
@@ -64,7 +64,7 @@ if (process.env.NODE_ENV === "production") {
 // Rate Limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
+  max: 300, // Limit each IP to 100 requests per windowMs
   message: "Too many requests from this IP, please try again later.",
 });
 app.use(limiter);
