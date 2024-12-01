25796e21e6bd62d85e9b018b7889b71e19a34c29
Om Prakash sah <omkolkata33@gmail.com>
2024-12-01 20:47:32 +0530

    Static pages content createor

--- src/Ver2Designs/Pages/index.ts
+++ src/Ver2Designs/Pages/index.ts
@@ -2,4 +2,5 @@ import Dashboard from "./Dashboard";
 import CreateCampaign from "./CreateCampaign/CreateCampaign";
 import PageNotfound from "./PageNotfound";
 import Landing from "./Landing"
-export { Dashboard , CreateCampaign , PageNotfound , Landing};
+export { default as ContentPage } from "./Content";
+export { Dashboard, CreateCampaign, PageNotfound, Landing };
