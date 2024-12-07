91defaf053fadbd8caf6c3020f8d7c5ee8364bfd
Om Prakash sah <omkolkata33@gmail.com>
2024-12-07 11:36:04 +0530

    Keys json file updated

--- buildContract.js
+++
@@ -1,7 +0,0 @@
-const {instantiateContract} = require("./initiateContract");
-
-// const proxyContract = fs.readFileSync("contract/HashbuzzProxy.sol", "utf8");
-// const logicalContract = fs.readFileSync("contract/HashbuzzLogicV1.sol", "utf8");
-
-const logicalContract =  instantiateContract("./contract/HashbuzzLogicV1.sol");
-console.log(logicalContract);
--- session-sec.js
+++ session-sec.js
@@ -1,8 +1,20 @@
 const crypto = require('crypto');
+const fs = require('fs');
 
 const generateRandomString = (length) => {
   return crypto.randomBytes(length).toString('hex');
 };
 
-const sessionSecret = generateRandomString(32);
-console.log(`SESSION_SECRET=${sessionSecret}`);
+const keysObj = {
+  J_ACCESS_TOKEN_SECRET: '',
+  J_REFRESH_TOKEN_SECRET: '',
+  ENCRYPTION_KEY: '',
+  SESSION_SECRET: '',
+};
+keysObj.J_ACCESS_TOKEN_SECRET = generateRandomString(16);
+keysObj.J_REFRESH_TOKEN_SECRET = generateRandomString(16);
+keysObj.ENCRYPTION_KEY = generateRandomString(20);
+keysObj.SESSION_SECRET = generateRandomString(32);
+
+fs.writeFileSync('keys.json', JSON.stringify(keysObj, null, 2));
+console.log('keys.json file has been saved.');
