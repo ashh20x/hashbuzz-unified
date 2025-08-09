b0c47bba646ecc66f742361b63b0c37d716d510e
Om prakash <omkolkata33@gmail.com>
2025-08-10 02:45:33 +0530

    feat: Replace static token data with dynamic account tokens retrieval and implement skip functionality

--- src/Ver2Designs/Pages/AuthAndOnboard/AssociateTokens/AssociateTokens.tsx
+++ src/Ver2Designs/Pages/AuthAndOnboard/AssociateTokens/AssociateTokens.tsx
@@ -1,12 +1,26 @@
+import { useGetAccountTokensQuery } from "@/API/mirrorNodeAPI";
 import PrimaryButtonV2 from "@/components/Buttons/PrimaryButtonV2";
+import { useAppDispatch, useAppSelector } from "@/Store/store";
 import { CheckCircleOutlineOutlined } from '@mui/icons-material';
 import { Box, Button } from "@mui/material";
+import { markAllTokensAssociated } from "../authStoreSlice";
 import SectionHeader from "../Components/SectionHeader/SectionHeader";
-import TookensData from "./data.json";
 import * as styles from "./styles";
 
 
 const AssociateTokens = () => {
+  const { wallet: { address }, token: { allAssociated, tokens } } = useAppSelector(state => state.auth.userAuthAndOnBoardSteps);
+  const { data: accountTokens } = useGetAccountTokensQuery(address, {
+    skip: !address,
+  });
+  const dispatch = useAppDispatch();
+
+  const handleSkipOrContinue = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    // Implement skip functionality
+    dispatch(markAllTokensAssociated());
+  };
+
   return (
     <Box sx={styles.associateTokensStyles}>
       <Box sx={styles.associateTokenWrapper}>
@@ -17,14 +31,14 @@ const AssociateTokens = () => {
         <Box sx={styles.listContainer}>
           <h4>Tokens List</h4>
           <Box>
-            {TookensData.map((token, index) => (
+            {tokens.map((token, index) => (
               <Box key={index} sx={styles.tokensListItem}>
                 <Box className="tokenIIcons">
-                  <span>{token.symbol}</span>
+                  <span>FG</span>
                 </Box>
                 <Box className="tokenDetails">
-                  <h4>{token.name}</h4>
-                  <p>{token.token_id}</p>
+                  {/* <h4>{token.name}</h4> */}
+                  <p>{token.token.token_id}</p>
                 </Box>
                 <Box className="linkOrStatus">
                   <Button startIcon={<CheckCircleOutlineOutlined />} variant="outlined" color="primary"  >
@@ -38,10 +52,10 @@ const AssociateTokens = () => {
 
       </Box>
       <Box sx={styles.associateTokenFooter}>
-        <Button variant="text" color="secondary" size="large">
+        <Button onClick={handleSkipOrContinue} variant="text" color="secondary" size="large">
           Skip
         </Button>
-        <PrimaryButtonV2 color="primary" aria-label="help">
+        <PrimaryButtonV2 onClick={handleSkipOrContinue} color="primary" aria-label="help">
           <span>Proceed</span>
         </PrimaryButtonV2>
       </Box>
